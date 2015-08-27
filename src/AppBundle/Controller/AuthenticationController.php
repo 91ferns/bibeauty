6817eccfee6eaa3a85d9f941b72195a867358b79
Stephen Parente <stephen@ingenuitydesign.com>
2015-08-27 17:23:14 -0400

    Front page styling getting done

---
+++ src/AppBundle/Controller/AuthenticationController.php
@@ -0,0 +1,43 @@
+<?php
+// src/AppBundle/Controller/AuthenticationController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class AuthenticationController extends Controller {
+
+    /**
+     * @Route("/login", name="login_route")
+     */
+    public function loginAction(Request $request) {
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'authentication/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction() {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+
+}
