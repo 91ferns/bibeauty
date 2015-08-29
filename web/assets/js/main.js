24a973628d70e1ac7b1306d966efbda0c1875a3e
Grennik <joey.ciervo@gmail.com>
2015-08-29 01:13:19 -0400

    Added jQuery to navbar

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,3 +1,15 @@
 jQuery(function($) {
 	console.log('test');
 });
+
+//window scroll function for navbar opacity
+
+jQuery(function($) {
+	$(window).scroll(function() {
+		if($(this).scrollTop() > 25){
+			$('.navbar-default').addClass('opaque');
+		} else {
+			$('.navbar-default').removeClass('opaque');
+		}
+	});
+});
