ff2a0fb75737b39a687de1f3db345a9de539ed84
pcaciula <pcaciula@gmail.com>
2015-09-05 00:03:42 +0000

    added search listings template, datepicker, and controller action for search_listings

--- src/AppBundle/Controller/ListingsController.php
+++ src/AppBundle/Controller/ListingsController.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -13,9 +15,26 @@ class ListingsController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
+        return $this->render('listings/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+    /**
+     * @Route("/listings_search", name="listings_search_path")
+     * @Method({"GET"})
+     */
+    public function searchAction(Request $request)
+    {
+        return $this->render('listings/search.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'results'  => ['business_name'=>'blah','star_img'=>'oops',
+                            'num_reviews'=>5, 'map_link'=>'x',
+                            'city'=>'x','state'=>'x','logo'=>'x',
+                            'description'=>'x','treatment'=>
+                                ['id'=>1,'name'=>'x',
+                                 'percent_discount'=>4,'start_dollars'=>20,
+                                 'num_remaining'=>4]
+                            ],
         ));
     }
 }
