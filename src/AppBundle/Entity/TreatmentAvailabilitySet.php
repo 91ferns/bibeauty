37bbc813816f71a65e019a41aeee5d839e5507ef
Stephen Parente <stephen@ingenuitydesign.com>
2015-09-16 14:45:32 -0400

    Done

--- src/AppBundle/Entity/TreatmentAvailabilitySet.php
+++ src/AppBundle/Entity/TreatmentAvailabilitySet.php
@@ -10,7 +10,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity()
  * @ORM\HasLifecycleCallbacks()
- * @ORM\Table(name="app_bookings")
+ * @ORM\Table(name="app_treatment_availability_sets")
  */
 class TreatmentAvailabilitySet {
     /**
