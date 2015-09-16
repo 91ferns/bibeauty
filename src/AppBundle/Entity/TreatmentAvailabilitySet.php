3547ab553254e8b3772a6ac41bbda142a3feb34f
paul <paul@ingenuitydesign.comgit config --global user.name Paul Caciula>
2015-09-16 17:19:22 -0400

    started schema for availability set

--- src/AppBundle/Entity/TreatmentAvailabilitySet.php
+++ src/AppBundle/Entity/TreatmentAvailabilitySet.php
@@ -4,8 +4,37 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Validator\Constraints as Assert;
 
 class TreatmentAvailabilitySet {
+  /**
+   * @ORM\Entity
+   * @ORM\Table(name="app_availabilityset")
+   */
+
+   /**
+    * @ORM\Column(type="integer")
+    * @ORM\Id
+    * @ORM\GeneratedValue(strategy="AUTO")
+    */
+   protected $id;
+
+   /**
+    * @ORM\Column(type="date")
+    * @Assert\NotBlank()
+    */
+   protected $date;
+
+   /**
+    * @ORM\Column(type="time", length=255)
+    * @Assert\NotBlank()
+    */
+   protected $time;
+
+   /**
+    * @ORM\Column(type="integer", options={"default": 0})
+    */
+   protected $isOpen;
+
 
 }
