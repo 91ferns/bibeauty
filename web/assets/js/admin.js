70eb81ad2cb9da64db10c0189f646fe89d19d28d
Stephen Parente <webmakersteve@gmail.com>
2015-11-18 23:33:45 -0500

    Some modularity

--- web/assets/js/admin.js
+++ web/assets/js/admin.js
@@ -2,7 +2,12 @@ jQuery(function($) {
   if ($('#BusinessesSelect')) {
     $('#BusinessesSelect').select2({
       theme: "bootstrap",
-       minimumResultsForSearch: 6
+      minimumResultsForSearch: 6,
+      placeholder: 'My Businesses',
     });
+    $('#BusinessesSelect').on('change', function() {
+      var val = $(this).val();
+      window.location.href = val;
+    })
   }
 });
