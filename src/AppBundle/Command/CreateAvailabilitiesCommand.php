ed65d56774c467b9b662d8ee179fd2d4fa8be34e
Stephen Parente <stephen@ingenuitydesign.com>
2015-12-17 12:12:42 -0500

    Updated command

--- src/AppBundle/Command/CreateAvailabilitiesCommand.php
+++ src/AppBundle/Command/CreateAvailabilitiesCommand.php
@@ -25,81 +25,95 @@ class CreateAvailabilitiesCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        return $this->getContainer()->get('old_sound_rabbit_mq.create_availabilities_producer')->publish(serialize($msg));
-
-        /*$pid = pcntl_fork();
-
-        if ($pid === -1) {
-            throw new \RuntimeException('Could not fork the process');
-        }*/
 
         $this->getContainer()->get('profiler')->disable();
         $availabilitySetId = $input->getArgument('id');
 
+        $logger = $this->getContainer()->get('logger');
         $em = $this->getContainer()->get('doctrine')->getManager();
         $em->getConnection()->getConfiguration()->setSQLLogger(null);
 
-        $logger = $this->getContainer()->get('logger');
+        try {
+            // $this->logger->addInfo('Start executing');
 
-        $offerAvailability = $em->getRepository("AppBundle:OfferAvailabilitySet");
+            echo 'WORKER: Creating availabilities for ' . $ID;
 
-        $availabilitySet = $offerAvailability->findOneById($availabilitySetId);
+            //Process picture upload.
+            //$msg will be an instance of `PhpAmqpLib\Message\AMQPMessage` with the $msg->body being the data sent over RabbitMQ.
 
-        if (!$availabilitySet) {
-            $logger->err('Could not find that availability set. Parammeters: ' . $availabilitySetId);
-            $output->writeln('ERR: Could not find that availability set');
-        }
-
-        $output->writeln('Executing creation of availability set');
-        $logger->info('Executing creation of availability set ' . $availabilitySetId);
-
-        // Start it going
-        $business = $availabilitySet->getTreatment()->getBusiness();
-
-        // Delete previous availabilities to make this rerunable
-        $queryBuilder = $em
-            ->createQueryBuilder()
-            ->delete('AppBundle:Availability', 'a')
-            ->innerJoin('a.availabilitySet', 's')
-            ->where('a.availabilitySet = :availabilitySet')
-            ->setParameter(':availabilitySet', $availabilitySet);
-
-        $queryBuilder->getQuery()->execute();
-
-        // Offer is made. We need to make its availability now
-        $matchingDates = $availabilitySet->datesThatMatchRecurrence();
-        // $availabilitySets = $availabilitySet->datesToAvailabilities($matchingDates, $business);
-        // $treatment = $availabilitySet->getTreatment();
-
-        $batchSize = 20;
-
-        foreach($matchingDates as $i => $date) {
-            $x = new \AppBundle\Entity\Availability();
-            $x->setDate($date);
-            $x->setAvailabilitySet($availabilitySet);
-            $x->setTreatment($availabilitySet->getTreatment());
-            $x->setBusiness($business);
-            $em->persist($x);
-
-            if ($i !== 0 && ($i % $batchSize) === 0) {
-                $em->flush();
-                $em->clear(); // Detaches objects
-
-                $availabilitySet = $offerAvailability->findOneById($availabilitySetId);
-                $business = $availabilitySet->getTreatment()->getBusiness();
-
+            if ($this->getContainer()->has('profiler')) {
+                $this->getContainer()->get('profiler')->disable();
             }
+
+            $offerAvailability = $em->getRepository("AppBundle:OfferAvailabilitySet");
+
+            $availabilitySet = $offerAvailability->findOneById($availabilitySetId);
+
+            if (!$availabilitySet) {
+                $msg = 'Could not find that availability set. Parameters: ' . $availabilitySetId;
+                throw new \Exception($msg, 0);
+            }
+
+            $logger->info('Executing creation of availability set ' . $availabilitySetId);
+
+            // Start it going
+            $business = $availabilitySet->getTreatment()->getBusiness();
+
+            // Delete previous availabilities to make this rerunable
+            $queryBuilder = $em
+                ->createQueryBuilder()
+                ->delete('AppBundle:Availability', 'a')
+                ->innerJoin('a.availabilitySet', 's')
+                ->where('a.availabilitySet = :availabilitySet')
+                ->setParameter(':availabilitySet', $availabilitySet);
+
+            $queryBuilder->getQuery()->execute();
+
+            // Offer is made. We need to make its availability now
+            $matchingDates = $availabilitySet->datesThatMatchRecurrence();
+            // $availabilitySets = $availabilitySet->datesToAvailabilities($matchingDates, $business);
+            // $treatment = $availabilitySet->getTreatment();
+
+            $batchSize = 20;
+
+            foreach($matchingDates as $i => $date) {
+                $x = new \AppBundle\Entity\Availability();
+                $x->setDate($date);
+                $x->setAvailabilitySet($availabilitySet);
+                $x->setTreatment($availabilitySet->getTreatment());
+                $x->setBusiness($business);
+                $em->persist($x);
+
+                if ($i !== 0 && ($i % $batchSize) === 0) {
+                    $em->flush();
+                    $em->clear(); // Detaches objects
+
+                    $availabilitySet = $offerAvailability->findOneById($availabilitySetId);
+                    $business = $availabilitySet->getTreatment()->getBusiness();
+
+                }
+            }
+
+            $availabilitySet->setProcessed(true);
+
+            $em->flush(); //Persist objects that did not make up an entire batch
+            $em->clear();
+            // End it
+
+            $text = 'Created ' . count($matchingDates) . ' availabilities.';
+            $logger->info($text);
+
+            //$this->container->get('api_mailer')->sendPrivatePathInvites($pathInvite);
+            /* end your code */
+            // $this->logger->addInfo('End executing');
+        } catch(\Exception $e) {
+            echo 'Failed';
+            $logger->addError($e->getMessage());
+
+            return $e->getCode();
         }
 
-        $availabilitySet->setProcessed(true);
+        return 0;
 
-        $em->flush(); //Persist objects that did not make up an entire batch
-        $em->clear();
-        // End it
-
-        $text = 'Created ' . count($matchingDates) . ' availabilities.';
-        $logger->info($text);
-
-        $output->writeln($text);
     }
 }
