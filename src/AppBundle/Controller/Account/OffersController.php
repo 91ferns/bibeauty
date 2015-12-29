4bd19f97814ff536608a53a605eef9216f8bb347
Stephen Parente <webmakersteve@gmail.com>
2015-12-29 14:02:03 -0500

    Requeue all route created

--- src/AppBundle/Controller/Account/OffersController.php
+++ src/AppBundle/Controller/Account/OffersController.php
@@ -63,6 +63,34 @@ class OffersController extends Controller implements AdminAwareController
     }
 
     /**
+     * @Route("/account/offers/requeue", name="admin_offer_requeue_path")
+     * @Method("GET")
+     */
+    public function requeueAllAction(Request $request)
+    {
+        $repo = $this->getRepo('OfferAvailabilitySet');
+        $avSets = $repo->findByProcessed(false);
+
+        $numAvailabilities = count($avSets);
+
+        if ($numAvailabilities > 0) {
+
+            foreach ($avSets as $avSet) {
+                $this->doAvailabilities($avSet->getId());
+            }
+
+            $this->addFlash(
+                'error',
+                'Queued creation of ' . $numAvailabilities . ' availabilities'
+            );
+
+        }
+
+        return $this->redirectToRoute('admin_businesses_path');
+
+    }
+
+    /**
      * @Route("/account/offers/{id}/{slug}/requeue/{offerId}", name="admin_business_offer_queue_path")
      * @Method("GET")
      */
