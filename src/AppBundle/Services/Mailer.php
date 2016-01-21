04389d14f074a74cd6559caffc56019837e8330e
Stephen Parente <webmakersteve@gmail.com>
2016-01-20 23:08:40 -0500

    Rest of the bugs

--- src/AppBundle/Services/Mailer.php
+++ src/AppBundle/Services/Mailer.php
@@ -49,7 +49,7 @@ class Mailer
           ->setSubject('You’ve received a new booking')
           ->setBody(
               $this->renderView(
-                'emails/business/booking-new.html.twig',
+                'emails/customer/booking-new.html.twig',
                 array(
                     'booking' => $booking,
                     'availability' => $availability,
